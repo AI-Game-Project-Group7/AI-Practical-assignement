abe48cf1efb6cb05396bea66d6999e62bcc9e316
AP2424 <68539131+AP2424@users.noreply.github.com>
2024-03-02 14:31:52 +0200

    The core of the game is done.

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ def generate_randoms(low=30000, high=50000, quantity=5):
         startnums.append(randint(low, high))
     return startnums
 
+# function which returns all possible divisors of a number
+def check_possible_divisors(num):
+    divisors = []
+    if num % 2 == 0:
+        divisors.append(2)
+        if num % 4 == 0:
+            divisors.append(4)
+    if num % 3 == 0:
+        divisors.append(3)
+    if num % 5 == 0:
+        divisors.append(5)
+    return sorted(divisors)
 
-
-
+# function which updates total points and bank points
+def update_points(num, pts, bankpts):
+    if num % 2 == 1:
+        pts += 1
+    else:
+        pts -= 1
+    if num % 10 in (0, 5):
+        bankpts += 1
+    return pts, bankpts
--- ui.py
+++ ui.py
@@ -1,39 +1,68 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QWidget, QStackedWidget
 from PyQt5.QtGui import QFont
 from main import *
+
+points = 0
+bankpoints = 0
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.points = 0
+        self.bankpoints = 0
+        self.stacked_widget = QStackedWidget(self)
+        self.startscreen = StartScreen(self.stacked_widget)
+        self.gamescreen = GameScreen(self.stacked_widget)
+        self.endscreen = EndScreen(self.stacked_widget)
+        self.stacked_widget.addWidget(self.startscreen)
+        self.stacked_widget.addWidget(self.gamescreen)
+        self.stacked_widget.addWidget(self.endscreen)
+        self.setCentralWidget(self.stacked_widget)
         self.setGeometry(500, 200, 600, 500)
         self.setWindowTitle("Game demonstration")
-        self.startscreen()
         self.show()
 
-    def startscreen(self):
+
+# insert styles for the game starting screen here
+class StartScreen(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget
         self.label = QLabel("Choose a starting number", self)
         self.label.setGeometry(150, 50, 350, 50)
         self.label.setFont(QFont('Arial', 20))
         numbers = generate_randoms()
         for i in range(len(numbers)):
             self.button = QPushButton(str(numbers[i]), self)
-            self.button.setGeometry(30+110*i, 150, 100, 40)
+            self.button.setGeometry(30 + 110 * i, 150, 100, 40)
         self.startbutton = QPushButton("Start", self)
         self.startbutton.setGeometry(200, 300, 200, 80)
-        self.startbutton.clicked.connect(self.on_startbutton_click)
-
-    def on_startbutton_click(self):
-        pass
-
-    def game_cycle(self):
-        pass
-    def endscreen(self):
-        pass
+        self.startbutton.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
 
 
+# insert styles for the game main screen here
+class GameScreen(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget
 
+
+# insert styles for the game end screen here
+class EndScreen(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget
+        label = QLabel("", self)
+        if points % 2:
+            label.setText("First player wins!")
+        else:
+            label.setText("Second player wins!")
+        label.setGeometry(150, 50, 350, 50)
+        label.setFont(QFont('Arial', 20))
+        newgamebutton = QPushButton("New game", self)
+        newgamebutton.setGeometry(200, 300, 200, 80)
+        newgamebutton.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
 
 app = QApplication(sys.argv)
 win = Window()
