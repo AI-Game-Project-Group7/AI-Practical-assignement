479eaeb541bd3a26ce2c18fb19f5bc2eb6eaeb98
Leyla Rahimova <Leilah.lite@gmail.com>
2024-03-28 16:06:06 +0200

    implement minimax and draft of hef

--- main.py
+++ main.py
@@ -60,45 +60,77 @@ def game_tree (node, depth, maxdepth=5):
         newpts, newbankpts = update_points(newnum, node.pts, node.bankpts)
 
         child_node = Node(newnum, newpts, newbankpts, depth + 1)
+        child_node.divisor = d
         node.add_child(child_node)
         nodes.append(child_node) # for display
 
         game_tree(child_node, depth + 1, maxdepth)
     return nodes # for display
 
-'''Depth is only useful to initalize root as a node with a depth of 0. It is not really meant to change so we could considere to delete it and only put values of 0 for the creation 
-of root. 
-However, if we want to change the depth of root, we can keep this implementation like that.'''
+
+def hef(node):
+    final_pts = node.pts + node.bankpts if node.pts % 2 == 0 else node.pts - node.bankpts
+    final_score_parity = final_pts % 2  
+    node.value = 0
+    num_divisors = len(check_possible_divisors(node.num))
+
+    if final_score_parity == 1:
+        node.value += -999 
+    else: 
+        node.value += 999   
+        
+    node.value += num_divisors
+    # node.value += node.depth
+
+    return node.value
+
+
+def minimax(node, is_max_turn):
+    if not node.children:  # If leaf node, return its heuristic value
+        node.value = hef(node)
+        return node.value
+
+    if is_max_turn:
+        # Maximizing player (human): looking for a high heuristic value
+        max_eval = float('-inf')
+        for child in node.children:
+            eval = minimax(child, False)
+            max_eval = max(max_eval, eval)
+        node.value = max_eval
+        return node.value
+    else:
+        # Minimizing player (computer): looking for a low heuristic value
+        min_eval = float('inf')
+        for child in node.children:
+            eval = minimax(child, True)
+            min_eval = min(min_eval, eval)
+        node.value = min_eval
+        return node.value
+
+
+def print_tree(node, level=0):
+    # Print the current node's details
+    print('  ' * level + f'Num: {node.num}, Pts: {node.pts}, BankPts: {node.bankpts}, Depth: {node.depth}, Value: {node.value}, Divisor: {getattr(node, "divisor", "N/A")}')
+    
+    # Recursively print each child
+    for child in node.children:
+        print_tree(child, level + 1)
+
 def choose_next_node(num, pts, bankpts, depth=0):
     root = Node(num, pts, bankpts, depth)
-    nodes = game_tree (root, depth)
+    nodes = game_tree(root, depth)
 
-    print(root.num, root.pts, root.bankpts, root.depth)
+    # Start minimax from the root node, assuming human starts the game
+    minimax(root, True)
+    print_tree(root)
 
-    for node in nodes: # for display
-        print(node.num, node.pts, node.bankpts, node.depth) # for display
-
-    return root
-
-choose_next_node(5000, -1, 1)
-
-def hef(nodes):
-    # abs(pts) + abs(bankpts) + numberofdivisors = odd - first players wins
-    # abs(pts) + abs(bankpts) + numberofdivisors  = even - second player wins
-    # insert heuristic evaluation function here
-    # for now first node is returned
-    return nodes[0]
+    # Choose the child of the root with the minimum value (computer's best move)
+    best_move = min(root.children, key=lambda x: x.value)
+    print(best_move.divisor)
+    return best_move.divisor
 
 
-# Hasan, you need to complete this algorithm
-# As input, only bottom level nodes have heuristic values,
-# For other nodes initial value is 0
-def minimax(nodes):
-    # the goal is to fill self.value parameter
-    # for the potential nodes which computer may choose
-    pass
-
-
+choose_next_node(300, -1, 1)
 
 
 
